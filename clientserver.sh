28b95e039a4956a6e45ed6e732792c7d26249d87
alari453 <alari453@student.otago.ac.nz>
2021-09-06 13:56:43 +1200

    web to client change

--- clientserver.sh
+++ clientserver.sh
@@ -1,12 +1,12 @@
 apt-get update
 apt-get install -y apache2 php libapache2-mod-php php-mysql
     
-# Change VM's webserver's configuration to use shared folder.
+# Change VM's clientserver's configuration to use shared folder.
 cp /vagrant/bookings-client.conf /etc/apache2/sites-available/
 
 # activate website configuration ...
 a2ensite bookings-client
 # ... and disable the default website provided with Apache
 a2dissite 000-default
-# Reload the webserver configuration, to pick up changes
+# Reload the clientserver configuration, to pick up changes
 service apache2 reload
