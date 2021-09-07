d81a9e44a83dd88cccacacc9061fe6831012992a
alari453 <alari453@student.otago.ac.nz>
2021-09-07 21:42:56 +1200

    created test values

--- setup-database.sql
+++ setup-database.sql
@@ -1,3 +1,19 @@
+DROP TABLE IF EXISTS scheduletest;
+
+-- /*initial test values*/
+--   CREATE TABLE scheduletest (
+--       bookingID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--       fName varchar(25) NOT NULL, 
+--       lName varchar(25) NOT NULL, 
+--       email varchar(100) NOT NULL, 
+--       bookingtTime varchar(10) NOT NULL UNIQUE
+--   );
+
+-- INSERT INTO scheduletest(fName, lName, email, bookingtTime)
+--              VALUES ('Jane', 'Doe', 'doeja@student.otago.ac.nz', '13:00');
+-- INSERT INTO scheduletest(fName, lName, email, bookingtTime)
+--               VALUES ('John', 'Doe', 'doejo@student.otago.ac.nz','14:00');
+
 DROP TABLE IF EXISTS schedule;
 
 /*information table for user's inpur*/
@@ -9,8 +25,7 @@ CREATE TABLE schedule(
   bookingtTime varchar(10) NOT NULL UNIQUE
 ) 
 
-/*auto generate/increment id number starting from 1000*/
-AUTO_INCREMENT=1000;
+AUTO_INCREMENT=1000; -- auto generate/increment bookingID number starting from 1000
 
 /*initial databse information*/
 INSERT INTO schedule (fName, lName, email, bookingtTime)
