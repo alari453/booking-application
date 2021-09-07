373a42493b09cb79488653bf149004c5a3830eea
teamwolf11 <mikecui5@gmail.com>
2021-09-07 17:24:37 +1200

    sdffsd

--- setup-database.sql
+++ setup-database.sql
@@ -7,13 +7,13 @@ CREATE TABLE schedule (
   fName varchar(25) NOT NULL, 
   lName varchar(25) NOT NULL, 
   email varchar(100) NOT NULL, 
-  preferredTime varchar(10) NOT NULL UNIQUE
+  time varchar(10) NOT NULL UNIQUE
 )AUTO_INCREMENT=1000;
-INSERT INTO schedule (fName, lName, email, preferredTime)
+INSERT INTO schedule (fName, lName, email, time)
              VALUES ('Riya', 'Alagh', 'alari@student.otago.ac.nz', '13:00');
-INSERT INTO schedule(fName, lName, email, preferredTime)
+INSERT INTO schedule(fName, lName, email, time)
               VALUES ('Mike', 'Cui', 'cuimi@student.otago.ac.nz','14:00');
-INSERT INTO schedule(fName, lName, email, preferredTime)
+INSERT INTO schedule(fName, lName, email, time)
               VALUES ('Scott', 'Smith', 'smisc@student.otago.ac.nz', '15:00');
-INSERT INTO schedule(fName, lName, email, preferredTime)
+INSERT INTO schedule(fName, lName, email, time)
               VALUES ('Maura', 'Higgins', 'higma53@student.otago.ac.nz','16:00');
